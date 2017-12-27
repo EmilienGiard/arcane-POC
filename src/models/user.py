e614c9df3a6739dfc983026afaf91264570610b0
Emilien <emilien.giard@gmail.com>
2017-12-27 13:55:33 +0100

    Add user model

---
+++ src/models/user.py
@@ -0,0 +1,30 @@
+"""
+Define the user model
+"""
+from . import db
+from .abc import BaseModel
+
+
+class User(db.Model, BaseModel):
+    """ The user model """
+    __tablename__ = 'user'
+
+    birth_date = db.Column(db.DateTime, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=True)
+    password = db.Column(db.String(100), nullable=True)
+
+    def __init__(self, birth_date, name, password):
+        """ Create a new user """
+        self.birth_date = birth_date
+        self.name = name
+        self.password = password
+
+    def to_dict(self):
+        """ Return a dict representation of the user model """
+        return {
+            'birth_date': self.birth_date.strftime('%m-%d-%Y'),
+            'id': self.id,
+            'name': self.name,
+            'password': self.password,
+        }
